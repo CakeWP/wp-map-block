ed77fba776b702045205bde159f36462b042ea98
Tushar Imran <tusharimran9909@gmail.com>
2021-09-29 19:52:37 +0600

    update version number

--- wp-map-block.php
+++ wp-map-block.php
@@ -6,7 +6,7 @@
  * Description: Gutenberg Map Block for Google Map and OpenStreet Map build with LeafletJS
  * Author: tusharimran
  * Author URI: http://itushar.me
- * Version: 1.2.4
+ * Version: 1.2.5
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
